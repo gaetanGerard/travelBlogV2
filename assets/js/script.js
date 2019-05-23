712ebba991a88da60b90181df7b619cc60da36a8
Gaétan <gaetan.gerard27@gmail.com>
2019-05-23 17:29:14 +0200

    step1 : barre de navigation pour smartphone (css,js,html) // complet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
+// ! Fonction pour ouverture et fermeture de la navBar responside (smartphone, tablette portrait, tablette paysage)
+
+
 let hamburger = document.getElementById('triggerHamburger');
-let navGroupContainer = document.getElementById("testId");
+let navGroupContainer = document.getElementById("navGroupContainer");
 
-let testFunction = () => {
+let openNavBar = () => {
     navGroupContainer.classList.toggle("active");
-}
+    if(navGroupContainer.classList.contains("active")) {
+        hamburger.classList.replace("hamburger", "closeHamburger");
+    }  else {
+        hamburger.classList.replace("closeHamburger", "hamburger");
+    }
+};
 
-hamburger.addEventListener("click", testFunction);
 
+hamburger.addEventListener("click", openNavBar);
+
+document.addEventListener("click", (evt) => {
+    let targetElement = evt.target;
+    let width = document.body.offsetWidth - targetElement.offsetWidth;
+
+    if (width === 0) {
+        console.log("offsetWidth = " + width);
+        hamburger.classList.replace("closeHamburger", "hamburger");
+        navGroupContainer.classList.remove("active");
+    } else if (targetElement == navGroupContainer) {
+        console.log("offsetWidth = " + width);
+        return;
+    }
+    console.log("offsetWidth = " + width);
+});

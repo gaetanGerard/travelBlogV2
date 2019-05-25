a7849b5857b5d27fcdbc2a6cc0227348ccb65769
Gaétan <gaetan.gerard27@gmail.com>
2019-05-25 15:43:39 +0200

    Complete the header/slider image with the js and styling just need to add an automatic slide on it

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,37 @@ let openNavBar = () => {
     }
 };
 
+let slideIndex = 1;
+
+let plusSlides = (n) => {
+    showSlides(slideIndex += n);
+}
+
+let currentSlide = (n) => {
+    showSlides(slideIndex = n);
+}
+
+let showSlides = (n) => {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+showSlides();
 
 hamburger.addEventListener("click", openNavBar);
 
@@ -20,7 +51,7 @@ document.addEventListener("click", (evt) => {
     let targetElement = evt.target;
     let width = document.body.offsetWidth - targetElement.offsetWidth;
 
-    if (width === 0) {
+    if (width === 0 || width < 0) {
         console.log("offsetWidth = " + width);
         hamburger.classList.replace("closeHamburger", "hamburger");
         navGroupContainer.classList.remove("active");

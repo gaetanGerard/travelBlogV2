58102309c19d05b67c843a9dd88f1b359ed96422
Gaétan <gaetan.gerard27@gmail.com>
2019-05-27 19:16:15 +0200

    Complete the aside style and logic on the smartphone device next step is the footer of this page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@
 
 let hamburger = document.getElementById('triggerHamburger');
 let navGroupContainer = document.getElementById("navGroupContainer");
+let popTrigger = document.getElementById("popTrigger");
+let recTrigger = document.getElementById("recTrigger");
+let populaire = document.getElementById("populaire");
+let recent = document.getElementById("recent");
+
+// * Fonction pour gérer l'ouverture et la fermeture du menu responsive
 
 let openNavBar = () => {
     navGroupContainer.classList.toggle("active");
@@ -12,6 +18,13 @@ let openNavBar = () => {
         hamburger.classList.replace("closeHamburger", "hamburger");
     }
 };
+
+
+// *
+// *
+// * Fonction pour gérer le défilement des slides 
+// *
+// * 
 
 let slideIndex = 1;
 
@@ -43,15 +56,49 @@ let showSlides = (n) => {
     dots[slideIndex-1].className += " active";
 };
 
+// *
+// *
+// * Fonction pour gérer le défilement des tab post récent et populaire
+// *
+// * 
+
+
+let showTabPop = () => {
+
+    if(!populaire.classList.contains("active")) {
+        populaire.classList.add("active");
+        recent.classList.remove("active");
+        popTrigger.classList.add("active");
+        recTrigger.classList.remove("active");
+    } 
+};
+
+let showTabRec = () => {
+
+    if (!recent.classList.contains("active")) {
+        recent.classList.add("active");
+        populaire.classList.remove("active");
+        recTrigger.classList.add("active");
+        popTrigger.classList.remove("active");
+    }
+};
+
 showSlides();
 
+
+
+popTrigger.addEventListener("click", showTabPop);
+recTrigger.addEventListener("click", showTabRec);
+
 hamburger.addEventListener("click", openNavBar);
+
+// ! event ayant pour but de fermer le menu responsive lorsque l'on clique en dehors du navbar
 
 document.addEventListener("click", (evt) => {
     let targetElement = evt.target;
     let width = document.body.offsetWidth - targetElement.offsetWidth;
 
-    if (width === 0 || width < 0) {
+    if (width === 0 || width < 0 || width < 130) {
         console.log("offsetWidth = " + width);
         hamburger.classList.replace("closeHamburger", "hamburger");
         navGroupContainer.classList.remove("active");
